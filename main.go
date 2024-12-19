a38857dcd7866601c075019041bc8a7499579182
dastarruer <ayushpramanik399@gmail.com>
2024-12-19 18:33:48 -0400

    CSS now applies correctly to site

--- main.go
+++ main.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"fmt"
-	// "html/template"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
 	// Initialize the server
-	fileServer := http.FileServer(http.Dir("./templates"))
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
 	// Handle all routes
-	http.Handle("/", fileServer)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+		tmpl.Execute(w, nil)
+	})
 
 	// Run the server
-	fmt.Printf("Starting server on port 8080...")
+	fmt.Printf("Running server on port http://localhost:8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
+
+
