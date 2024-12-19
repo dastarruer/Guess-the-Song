731696089b62ac61858212a2437e60fd8fdd613f
dastarruer <ayushpramanik399@gmail.com>
2024-12-19 17:09:47 -0400

    Server can now be run with main.go

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-import(
+import (
+	"fmt"
+	// "html/template"
+	"log"
 	"net/http"
-	"html/template"
 )
+
+func main() {
+	// Initialize the server
+	fileServer := http.FileServer(http.Dir("./templates"))
+
+	// Handle all routes
+	http.Handle("/", fileServer)
+
+	// Run the server
+	fmt.Printf("Starting server on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
