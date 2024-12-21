fbbd8484c4b66e261f0e7578d9cf7f59c537ecf4
dastarruer <ayushpramanik399@gmail.com>
2024-12-21 18:44:43 -0400

    Added constant for billboard top 100 playlist id

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,5 @@
 const apiButton = document.getElementById("api");
+const billboardTop100PlaylistID = "6UeSakyzhiEt4NB3UAd6NQ?si=fb9ea777334348d1";
 
 apiButton.addEventListener("click", async () => {
     const accessToken = await getAccessToken();
