7d39ba434c1b0dc02fcc26c6c01ce040b04c2f12
dastarruer <ayushpramanik399@gmail.com>
2024-12-20 20:19:47 -0400

    Moved index.html to static/ as it is a static file

--- handlers/index_handler.go
+++ handlers/index_handler.go
@@ -1,14 +1,9 @@
 package handlers
 
 import (
-	"html/template"
 	"net/http"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	// Get the index.html template
-	tmpl := template.Must(template.ParseFiles("./templates/index.html"))
-
-	// Show the page to the user
-	tmpl.Execute(w, nil)
+	http.ServeFile(w, r, "./static/index.html")
 }
