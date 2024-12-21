1c226aabc1102e6264a3555631cd2dae672b6b1e
dastarruer <ayushpramanik399@gmail.com>
2024-12-21 18:25:51 -0400

    Js will now make a request for access token

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,11 @@
 const apiButton = document.getElementById("api");
 
-apiButton.addEventListener("click", () => {
-    console.log("button clicked");
+apiButton.addEventListener("click", async () => {
+    const accessToken = await getAccessToken();
 });
+
+async function getAccessToken() {
+    const response = await fetch("http://localhost:8080/auth");
+    const data = await response.json();
+    return data.access_token;
+}
