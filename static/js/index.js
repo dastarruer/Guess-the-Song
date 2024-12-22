ad36af3cf877d569e3f857a1992b25d4001cc705
dastarruer <ayushpramanik399@gmail.com>
2024-12-21 20:33:02 -0400

    Abstracted funciton to get json from backend to a function

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,12 @@
 const apiButton = document.getElementById("api");
 
 apiButton.addEventListener("click", async () => {
-    const response = await fetch("http://localhost:8080/auth");
-    const data = await response.json()
-    console.log(data)
+    const data = await getRandomBillboardHot100Song();
+    console.log(data);
 });
 
+async function getRandomBillboardHot100Song() {
+    const response = await fetch("http://localhost:8080/auth");
+    const data = await response.json();
+    return data;
+}
