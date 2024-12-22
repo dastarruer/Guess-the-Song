abfdaad927823ef05b696e8bad6775a24c84c10c
dastarruer <ayushpramanik399@gmail.com>
2024-12-21 22:31:59 -0400

    JS now gets a random track from the response

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,4 @@
+// TODO: Play a random song off of the billboard hot 100
 const apiButton = document.getElementById("api");
 
 apiButton.addEventListener("click", async () => {
@@ -8,5 +9,7 @@ apiButton.addEventListener("click", async () => {
 async function getRandomBillboardHot100Song() {
     const response = await fetch("http://localhost:8080/auth");
     const data = await response.json();
-    return data;
+    const track =
+        data.tracks.items[Math.floor(Math.random() * data.tracks.items.length)];
+    return track;
 }
