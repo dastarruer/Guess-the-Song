b9d3c735ac7787b9ece9a9ed4b211e33d5cd4800
dastarruer <ayushpramanik399@gmail.com>
2024-12-28 17:24:17 -0400

    Frontend now converts data to json

--- static/js/index.js
+++ static/js/index.js
@@ -2,5 +2,6 @@ const apiButton = document.getElementById("api");
 
 apiButton.addEventListener("click", async () => {
     const data = await fetch("http://localhost:8080/track");
-    console.log(data);
+    const json = await data.json()
+    console.log(json);
 });
