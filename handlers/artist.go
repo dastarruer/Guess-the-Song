f777acf0af13219b1c18325b897deb1cbc7ee050
dastarruer <ayushpramanik399@gmail.com>
2024-12-28 19:36:41 -0400

    have no idea what i did but fixed json bad parse error

--- handlers/artist.go
+++ handlers/artist.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"encoding/json"
+	// "fmt"
 	"net/http"
 
 	"github.com/carlmjohnson/requests"
@@ -15,12 +16,12 @@ type Artist struct {
 
 func ArtistHandler(w http.ResponseWriter, r *http.Request) {
 	id := 27
-	var track Artist
+	var artist Artist
 	err := requests.
 		URL("https://api.deezer.com").
 		Pathf("/artist/%d", id).
 		ContentType("application/json").
-		ToJSON(&track).
+		ToJSON(&artist).
 		Fetch(context.Background())
 	if err != nil {
 		http.Error(w, "could not connect to jsonplaceholder.typicode.com:", http.StatusInternalServerError)
@@ -30,7 +31,7 @@ func ArtistHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// Write the track object as JSON to the response
-	if err := json.NewEncoder(w).Encode(track); err != nil {
+	if err := json.NewEncoder(w).Encode(artist); err != nil {
 		http.Error(w, "could not encode track data to JSON", http.StatusInternalServerError)
 		return
 	}
--- static/js/index.js
+++ static/js/index.js
@@ -2,15 +2,15 @@ const apiButton = document.getElementById("api");
 const trackPlayer = document.getElementById("track");
 
 apiButton.addEventListener("click", async () => {
-    const json = await getArtist()
-    console.log(json)
+    const json = await getArtist();
+    console.log(json);
 });
 
 // TODO: fix error (json bad parse)
 async function getArtist() {
     const data = await fetch("http://localhost:8080/artist");
-    const json = await data.json()
-    return json
+    const json = await data.json();
+    return json;
 }
 
 async function setTrack() {
