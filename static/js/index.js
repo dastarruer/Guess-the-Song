697e0d81e338b5c4c06a35deecca57911127cce3
dastarruer <ayushpramanik399@gmail.com>
2024-12-28 18:06:37 -0400

    refactored setting track to its own function, fetch artist json from frontend and log it

--- static/js/index.js
+++ static/js/index.js
@@ -2,9 +2,21 @@ const apiButton = document.getElementById("api");
 const trackPlayer = document.getElementById("track");
 
 apiButton.addEventListener("click", async () => {
+    const json = await getArtist()
+    console.log(json)
+});
+
+// TODO: fix error (json bad parse)
+async function getArtist() {
+    const data = await fetch("http://localhost:8080/artist");
+    const json = await data.json()
+    return json
+}
+
+async function setTrack() {
     const data = await fetch("http://localhost:8080/track");
     const json = await data.json();
 
     trackPlayer.src = json.preview;
-    trackPlayer.load()
-});
+    trackPlayer.load();
+}
