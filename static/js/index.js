dee05a84b7e7113b3a5b6f3588f130c7273ab169
dastarruer <ayushpramanik399@gmail.com>
2024-12-30 12:02:04 -0400

    moved distance function to index.js

--- static/js/index.js
+++ static/js/index.js
@@ -28,6 +28,35 @@ window.onload = async function () {
     })
 };
 
+function levenshteinDistance(a, b) {
+    let lenA = a.length;
+    let lenB = b.length;
+
+    let headA = a[0];
+    let headB = b[0];
+
+    let tailA = a.slice(1);
+    let tailB = b.slice(1);
+
+    if (lenA === 0) {
+        return lenB;
+    } else if (lenB === 0) {
+        return lenA;
+    }
+
+    if (headA === headB) {
+        return levenshteinDistance(tailA, tailB);
+    }
+
+    return (
+        Math.min(
+            levenshteinDistance(tailA, b),
+            levenshteinDistance(a, tailB),
+            levenshteinDistance(tailA, tailB)
+        ) + 1
+    );
+}
+
 function getRandomArtistTrack(tracklist) {
     let track = tracklist[Math.floor(Math.random() * tracklist.length)];
     return track;
