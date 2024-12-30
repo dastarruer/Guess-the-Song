08a8c4bdd44c42b93411ea40a0c9a65c4e64de95
dastarruer <ayushpramanik399@gmail.com>
2024-12-30 11:47:08 -0400

    added base cases

--- static/js/levenshetein_distance.js
+++ static/js/levenshetein_distance.js
@@ -1,4 +1,11 @@
 function levenshteinDistance(a, b) {
     lenA = a.length;
     lenB = b.length;
+
+    if (lenA === 0) {
+        return lenB;
+    } else if (lenB === 0) {
+        return lenA;
+    }
 }
+console.log(levenshteinDistance("", "hello"));
