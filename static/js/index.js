4e0e636989ce170efe0dc1bbb737a5fb8b2f8bcc
dastarruer <ayushpramanik399@gmail.com>
2025-01-17 23:32:44 -0400

    suggestions now work

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-import songSuggestions from "./suggestions";
+import SuggestionProvider from "./suggestions.js";
 
 const submitButton = document.getElementById("submit-guess");
 const guessInput = document.getElementById("input");
@@ -7,7 +7,8 @@ window.onload = async function () {
     const tracklist = await getArtistTracklist();
     const track = getRandomArtistTrack(tracklist);
     const artist = await getArtist();
-    songSuggestions.h()
+
+    const songSuggestions = new SuggestionProvider();
 
     let relevantTracks = null;
 
@@ -28,52 +29,9 @@ window.onload = async function () {
         }
     });
 
-    guessInput.addEventListener(
-        "input",
-        debounce(() => {
-            const guess = guessInput.value.toLowerCase();
-            relevantTracks = tracklist.filter((track) =>
-                track.title.toLowerCase().startsWith(guess.slice(0, 3))
-            );
-            relevantTracks = tracklist
-                .sort((a, b) => {
-                    // Calculate Levenshtein distance for both tracks' names
-                    const distanceA = levenshteinDistance(
-                        guess,
-                        a.title.toLowerCase()
-                    );
-                    const distanceB = levenshteinDistance(
-                        guess,
-                        b.title.toLowerCase()
-                    );
-
-                    // Compare distances
-                    return distanceA - distanceB;
-                })
-                .slice(0, 3);
-            const suggestions = document.getElementById(
-                "suggestions-container"
-            );
-
-            // Clear the list so that the previous suggestions are not shown
-            suggestions.innerHTML = "";
-            suggestions.style.display = "block";
-            for (const track of relevantTracks) {
-                const suggestion = document.createElement("li");
-                suggestion.innerHTML = `
-                    <li tabindex="0" class="suggestion-container">
-                        <img class="suggestion-cover" src=${track.album.cover}>
-                        <div class="suggestion-caption-container">
-                            <p class="suggestion-title">${track.title}</p>
-                            <p class="suggestion-artist">${artist.name}</p>
-                        </div>
-                    </li>
-                `;
-                suggestions.appendChild(suggestion);
-            }
-        }),
-        300
-    );
+    guessInput.addEventListener("input", () => {
+        songSuggestions.showSuggestions(guessInput, artist, tracklist);
+    });
 };
 
 function debounce(func, delay) {
@@ -82,31 +40,6 @@ function debounce(func, delay) {
         clearTimeout(timer);
         timer = setTimeout(() => func.apply(this, args), delay);
     };
-}
-
-function levenshteinDistance(a, b) {
-    const lenA = a.length,
-        lenB = b.length;
-
-    const d = Array.from({ length: lenA + 1 }, () => Array(lenB + 1).fill(0));
-
-    for (let i = 0; i <= lenA; i++) d[i][0] = i;
-    for (let j = 0; j <= lenB; j++) d[0][j] = j;
-
-    for (let i = 1; i <= lenA; i++) {
-        for (let j = 1; j <= lenB; j++) {
-            if (a[i - 1] === b[j - 1]) {
-                d[i][j] = d[i - 1][j - 1];
-            } else {
-                d[i][j] = Math.min(
-                    d[i - 1][j] + 1, // Deletion
-                    d[i][j - 1] + 1, // Insertion
-                    d[i - 1][j - 1] + 1 // Substitution
-                );
-            }
-        }
-    }
-    return d[lenA][lenB];
 }
 
 function getRandomArtistTrack(tracklist) {
--- static/js/suggestions.js
+++ static/js/suggestions.js
@@ -1,7 +1,71 @@
 class SuggestionProvider {
-    h() {
-        console.log("h");
+    showSuggestions(guessInput, artist, tracklist) {
+        const guess = guessInput.value.toLowerCase();
+        let relevantTracks = tracklist.filter((track) =>
+            track.title.toLowerCase().startsWith(guess.slice(0, 3))
+        );
+        relevantTracks = tracklist
+            .sort((a, b) => {
+                // Calculate Levenshtein distance for both tracks' names
+                const distanceA = this.levenshteinDistance(
+                    guess,
+                    a.title.toLowerCase()
+                );
+                const distanceB = this.levenshteinDistance(
+                    guess,
+                    b.title.toLowerCase()
+                );
+
+                // Compare distances
+                return distanceA - distanceB;
+            })
+            .slice(0, 3);
+        const suggestions = document.getElementById("suggestions-container");
+
+        // Clear the list so that the previous suggestions are not shown
+        suggestions.innerHTML = "";
+        suggestions.style.display = "block";
+        for (const track of relevantTracks) {
+            console.log("adding suggestion")
+            const suggestion = document.createElement("li");
+            suggestion.innerHTML = `
+                    <li tabindex="0" class="suggestion-container">
+                        <img class="suggestion-cover" src=${track.album.cover}>
+                        <div class="suggestion-caption-container">
+                            <p class="suggestion-title">${track.title}</p>
+                            <p class="suggestion-artist">${artist.name}</p>
+                        </div>
+                    </li>
+                `;
+            suggestions.appendChild(suggestion);
+        }
+    }
+
+    levenshteinDistance(a, b) {
+        const lenA = a.length,
+            lenB = b.length;
+
+        const d = Array.from({ length: lenA + 1 }, () =>
+            Array(lenB + 1).fill(0)
+        );
+
+        for (let i = 0; i <= lenA; i++) d[i][0] = i;
+        for (let j = 0; j <= lenB; j++) d[0][j] = j;
+
+        for (let i = 1; i <= lenA; i++) {
+            for (let j = 1; j <= lenB; j++) {
+                if (a[i - 1] === b[j - 1]) {
+                    d[i][j] = d[i - 1][j - 1];
+                } else {
+                    d[i][j] = Math.min(
+                        d[i - 1][j] + 1, // Deletion
+                        d[i][j - 1] + 1, // Insertion
+                        d[i - 1][j - 1] + 1 // Substitution
+                    );
+                }
+            }
+        }
+        return d[lenA][lenB];
     }
 }
-let songSuggestions = new SuggestionProvider()
-export default suggestions;
+export default SuggestionProvider;
