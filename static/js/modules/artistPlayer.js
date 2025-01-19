423b2e1dddd7e4fdf85ba17399a6d36d1e3384b8
dastarruer <ayushpramanik399@gmail.com>
2025-01-19 11:30:48 -0400

    added more comments

--- static/js/modules/artistPlayer.js
+++ static/js/modules/artistPlayer.js
@@ -6,11 +6,13 @@ class ArtistPlayer {
         this.trackNameElement = document.getElementById("track-name");
     }
 
+    // Fetch the artist's data from the /artist API endpoint
     async fetchArtist() {
         const data = await fetch("http://localhost:8080/artist");
         this.artist = await data.json();
     }
 
+    // Fetch the artist's top songs' data from the /artist/top API endpoint
     async fetchTracklist() {
         const data = await fetch("http://localhost:8080/artist/top");
         const json = await data.json();
@@ -18,15 +20,23 @@ class ArtistPlayer {
         this.tracklist = json.data;
     }
 
-    async fetchRandomArtistTrack() {
+    // Fetch a random track from the tracklist fetched in this.fetchTracklist()
+    fetchRandomArtistTrack() {
         this.track =
             this.tracklist[Math.floor(Math.random() * this.tracklist.length)];
     }
+
+    /**
+     * Asynchronously sets the current track and artist information.
+     * Fetches artist data, tracklist, and a random track, then updates the audio source and
+     * artist elements in the DOM. The track name is obscured as "???" until guessed correctly.
+     */
 
     async setTrack() {
         await this.fetchArtist();
         await this.fetchTracklist();
         await this.fetchRandomArtistTrack();
+
         // Set the audio
         this.trackPlayer.src = this.track.preview;
         this.trackPlayer.load();
