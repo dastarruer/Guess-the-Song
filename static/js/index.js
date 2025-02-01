46069681aab3b770c56c1c31984f8da68875bce7
dastarruer <ayushpramanik399@gmail.com>
2025-01-31 20:20:51 -0400

    create a suggestion index that is moved using up/down keys

--- static/js/index.js
+++ static/js/index.js
@@ -3,6 +3,8 @@ import ArtistPlayer from "./modules/artistPlayer.js";
 
 const submitButton = document.getElementById("submit-guess");
 const guessInput = document.getElementById("input");
+const suggestionItems = document.querySelectorAll(".suggestion-container");
+let currentSuggestionIndex = -1;
 
 window.onload = async function () {
     const player = new ArtistPlayer();
@@ -31,5 +33,30 @@ window.onload = async function () {
 
     guessInput.addEventListener("input", () => {
         songSuggestions.showSuggestions();
+    });
+
+    document.addEventListener("keydown", () => {
+        let firstSuggestionIndex = 0;
+        let lastSuggestionIndex = 2;
+        let defaultSuggestionIndex = -1;
+
+        // Function to handle suggestion navigation
+        function updateSuggestionIndex(event) {
+            if (event.key === "ArrowUp") {
+                currentSuggestionIndex = Math.max(
+                    firstSuggestionIndex,
+                    currentSuggestionIndex - 1
+                );
+            } else if (event.key === "ArrowDown") {
+                currentSuggestionIndex = Math.min(
+                    lastSuggestionIndex,
+                    currentSuggestionIndex + 1
+                );
+            } else if (currentSuggestionIndex < defaultSuggestionIndex) {
+                currentSuggestionIndex = firstSuggestionIndex;
+            }
+        }
+
+        document.addEventListener("keydown", updateSuggestionIndex);
     });
 };
