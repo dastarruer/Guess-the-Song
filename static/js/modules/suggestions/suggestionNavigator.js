90e8efa1e2b6d40df9e296ad6316a5a6c96f6d0a
dastarruer <ayushpramanik399@gmail.com>
2025-04-05 11:23:50 -0400

    Move suggestion indexes to global scope in SuggestionNavigator

--- static/js/modules/suggestions/suggestionNavigator.js
+++ static/js/modules/suggestions/suggestionNavigator.js
@@ -1,6 +1,11 @@
 class SuggestionNavigator {
     constructor() {
         this.currentSuggestionIndex = -1;
+        this.defaultSuggestionIndex = -1;
+        this.firstSuggestionIndex = 0;
+
+        // Will be modified depending on the number of suggestions to navigate
+        this.lastSuggestionIndex = null;
     }
 
     /** Gets all the suggestions elements */
@@ -11,23 +16,25 @@ class SuggestionNavigator {
         return suggestionItems;
     }
 
+    /** Navigate suggestions based on the key pressed.
+     * If the user presses the down key, highlights the suggestion below the current one.
+     * If the user presses the up key, highlights the suggestion above the current one.
+     */
     navigateSuggestions(event, numSuggestions) {
-        // If there are no suggestions to navigate, return
+        // If there are no suggestions to navigate, terminate early
         if (numSuggestions === 0) {
             return;
         }
 
         let suggestionItems = this.getSuggestionElements();
 
-        let defaultSuggestionIndex = -1;
-        let firstSuggestionIndex = 0;
         let lastSuggestionIndex = numSuggestions - 1;
 
-        if (this.currentSuggestionIndex < defaultSuggestionIndex) {
-            this.currentSuggestionIndex = firstSuggestionIndex;
+        if (this.currentSuggestionIndex < this.defaultSuggestionIndex) {
+            this.currentSuggestionIndex = this.firstSuggestionIndex;
         } else if (event.key === "ArrowUp") {
             // Check if the currentSuggestionIndex is within the bounds of the suggestion list
-            if (this.currentSuggestionIndex > firstSuggestionIndex) {
+            if (this.currentSuggestionIndex > this.firstSuggestionIndex) {
                 // Remove the highlight of the current suggestion
                 suggestionItems[this.currentSuggestionIndex].classList.remove(
                     "highlight"
@@ -36,7 +43,7 @@ class SuggestionNavigator {
 
             // This makes sure that this.currentSuggestionIndex never goes under firstSuggestionIndex
             this.currentSuggestionIndex = Math.max(
-                firstSuggestionIndex,
+                this.firstSuggestionIndex,
                 this.currentSuggestionIndex - 1
             );
 
@@ -48,7 +55,7 @@ class SuggestionNavigator {
             // Check if the currentSuggestionIndex is within the bounds of the suggestion list
             if (
                 this.currentSuggestionIndex < lastSuggestionIndex &&
-                this.currentSuggestionIndex >= firstSuggestionIndex
+                this.currentSuggestionIndex >= this.firstSuggestionIndex
             ) {
                 // Remove highlight on the current suggestion
                 suggestionItems[this.currentSuggestionIndex].classList.remove(
@@ -79,118 +86,3 @@ class SuggestionNavigator {
 }
 
 export default SuggestionNavigator;
-export class SuggestionProvider {
-    /**
-     * Initializes a new instance of the SuggestionProvider class. This class will provide suggestions to the user based on what they type into text boxes.
-     * @param {Object} artist - The artist object containing artist details.
-     * @param {Array} tracklist - The list of tracks associated with the artist.
-     */
-    constructor(artist, tracklist) {
-        this.artist = artist;
-        this.tracklist = tracklist;
-
-        this.relevantTracks = null;
-    }
-
-    /** Gets all the suggestions elements */
-    getSuggestionElements() {
-        let suggestionItems = document.getElementsByClassName(
-            "suggestion-container"
-        );
-        return suggestionItems;
-    }
-
-    /**
-     * Displays song suggestions based on the user's input.
-     * Filters the tracklist to find tracks whose titles start with the first
-     * three characters of the user's guess, then sorts them by their
-     * Levenshtein distance to the guess. The top three closest matches are
-     * displayed in the suggestions container. Each suggestion includes the
-     * track's cover image, title, and artist's name.
-     */
-    showSuggestions(guess) {
-        this.getRelevantTracks(guess);
-
-        this.appendSuggestionsToHTML();
-    }
-
-    appendSuggestionsToHTML() {
-        // The container that all suggestions are shown in
-        const suggestions = document.getElementById("suggestions-container");
-
-        // Clear the list so that the previous suggestions are not shown
-        suggestions.innerHTML = "";
-
-        // Show the suggestions
-        suggestions.style.display = "block";
-
-        for (const track of this.relevantTracks) {
-            // Create a list element to show the track
-            const suggestion = document.createElement("li");
-            suggestion.classList.add("suggestion-container");
-
-            // The HTML of the suggestion
-            suggestion.innerHTML = `
-                    <img class="suggestion-cover" src=${track.album.cover}>
-                    <div class="suggestion-caption-container">
-                        <p class="suggestion-title martian-mono">${track.title}</p>
-                        <p class="suggestion-artist semibold">${this.artist.name}</p>
-                    </div>
-                `;
-            suggestions.appendChild(suggestion);
-        }
-    }
-
-    /** Set the relevant tracks to the first 3 characters of each track's title. */
-    getRelevantTracks(guess) {
-        this.relevantTracks = this.tracklist.filter((track) =>
-            track.title.toLowerCase().startsWith(guess.slice(0, 3))
-        );
-
-        // Sort the tracks from smallest to biggest distance from the guess
-        this.relevantTracks = this.relevantTracks
-            .sort((a, b) => {
-                // Calculate Levenshtein distance for both tracks' names, and whittle it down to the first three tracks
-                const distanceA = this.levenshteinDistance(
-                    guess,
-                    a.title.toLowerCase()
-                );
-                const distanceB = this.levenshteinDistance(
-                    guess,
-                    b.title.toLowerCase()
-                );
-
-                // Compare distances
-                return distanceA - distanceB;
-            })
-            .slice(0, 3); // Take the first 3 characters of the string
-    }
-
-    // Calculate the levenshtein distance between two given strings
-    levenshteinDistance(a, b) {
-        const lenA = a.length,
-            lenB = b.length;
-
-        const d = Array.from({ length: lenA + 1 }, () =>
-            Array(lenB + 1).fill(0)
-        );
-
-        for (let i = 0; i <= lenA; i++) d[i][0] = i;
-        for (let j = 0; j <= lenB; j++) d[0][j] = j;
-
-        for (let i = 1; i <= lenA; i++) {
-            for (let j = 1; j <= lenB; j++) {
-                if (a[i - 1] === b[j - 1]) {
-                    d[i][j] = d[i - 1][j - 1];
-                } else {
-                    d[i][j] = Math.min(
-                        d[i - 1][j] + 1, // Deletion
-                        d[i][j - 1] + 1, // Insertion
-                        d[i - 1][j - 1] + 1 // Substitution
-                    );
-                }
-            }
-        }
-        return d[lenA][lenB];
-    }
-}
