71b369a48359dd8b5a90b7f24c7263cfbdcb34e6
dastarruer <ayushpramanik399@gmail.com>
2025-04-05 19:36:08 -0400

    Add docstrings

--- static/js/index.js
+++ static/js/index.js
@@ -54,10 +54,11 @@ function handleIncorrectGuess(firstGuessElement) {
     firstGuessElement.className = "incorrect-guess";
 }
 
+/**Handles logic if user guesses correctly.
+ * Will firstly show a checkmark to indicate that the user has correctly guessed.
+ * Then, the function will reveal the track name and album to the user.
+ */
 function handleCorrectGuess(firstGuessElement, trackName) {
-    const trackNameElement = document.getElementById("track-name");
-    const artistImageElement = document.getElementById("artist-image");
-
     const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
                     <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
                 </svg>`;
@@ -66,16 +67,22 @@ function handleCorrectGuess(firstGuessElement, trackName) {
     firstGuessElement.className = "correct-guess";
     firstGuessElement.innerHTML = checkIcon;
 
-    revealTrack(trackNameElement, trackName, artistImageElement);
+    revealTrack(trackName);
 }
 
-function revealTrack(trackNameElement, trackName, artistImageElement) {
+/** Show the user the track name and album cover of the song. */
+function revealTrack(trackName) {
+    const trackNameElement = document.getElementById("track-name");
+    const artistImageElement = document.getElementById("artist-image");
+
     trackNameElement.textContent = trackName;
     artistImageElement.src = track.album.cover;
 }
 
+/** If the given guess is equal to the track name (disregarding case),
+ * and there is not currently a correct guess, or the user has run out of guesses */
 function verifyGuess(guess, trackName) {
-    const correctGuessExists = hasCorrectGuess();
+    const correctGuessExists = isGuessLeft();
 
     if (guess === trackName.toLowerCase() && !correctGuessExists) {
         return true;
@@ -84,6 +91,8 @@ function verifyGuess(guess, trackName) {
     }
 }
 
-function hasCorrectGuess() {
+/** Check if there is a correct-guess element.
+ * This tells us that the user cannot guess again */
+function isGuessLeft() {
     return document.querySelector(".correct-guess") !== null;
 }
