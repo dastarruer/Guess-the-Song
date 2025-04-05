e9c01309c8365c36297551e48fa76b0d465d5d2b
dastarruer <ayushpramanik399@gmail.com>
2025-04-05 19:51:03 -0400

    Refactor game logic to GameManager class

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,7 @@ import ArtistPlayer from "./modules/artistPlayer.js";
 import SuggestionRenderer from "./modules/suggestions/suggestionRenderer.js";
 import SuggestionNavigator from "./modules/suggestions/suggestionNavigator.js";
 import TrackMatcher from "./modules/suggestions/trackMatcher.js";
+import GameManager from "./modules/gameManager.js";
 
 const submitButton = document.getElementById("submit-guess");
 const guessInput = document.getElementById("input");
@@ -14,8 +15,10 @@ window.onload = async function () {
     const trackMatcher = new TrackMatcher(player.tracklist);
     const songSuggestions = new SuggestionRenderer(player.artist);
 
+    const gameManager = new GameManager();
+
     submitButton.addEventListener("click", () => {
-        verifyGuess(player);
+        gameManager.verifyGuess(guessInput.value.toLowerCase());
     });
 
     document.addEventListener("keydown", (event) => {
@@ -40,59 +43,14 @@ window.onload = async function () {
         if (event.key === "Enter") {
             const firstGuessElement = document.querySelector(".guess");
             const trackName = player.track.title;
+            const trackCoverUrl = player.track.album.cover;
 
-            if (verifyGuess(guessInput.value.toLowerCase(), trackName)) {
-                handleCorrectGuess(firstGuessElement, trackName);
+            if (gameManager.verifyGuess(guessInput.value.toLowerCase(), trackName)) {
+                gameManager.handleCorrectGuess(firstGuessElement, trackName, trackCoverUrl);
             } else {
-                handleIncorrectGuess(firstGuessElement);
+                gameManager.handleIncorrectGuess(firstGuessElement);
             }
         }
     });
 };
 
-function handleIncorrectGuess(firstGuessElement) {
-    firstGuessElement.className = "incorrect-guess";
-}
-
-/**Handles logic if user guesses correctly.
- * Will firstly show a checkmark to indicate that the user has correctly guessed.
- * Then, the function will reveal the track name and album to the user.
- */
-function handleCorrectGuess(firstGuessElement, trackName) {
-    const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-                    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
-                </svg>`;
-
-    // Show a green check to indicate that the user made a correct guess
-    firstGuessElement.className = "correct-guess";
-    firstGuessElement.innerHTML = checkIcon;
-
-    revealTrack(trackName);
-}
-
-/** Show the user the track name and album cover of the song. */
-function revealTrack(trackName) {
-    const trackNameElement = document.getElementById("track-name");
-    const artistImageElement = document.getElementById("artist-image");
-
-    trackNameElement.textContent = trackName;
-    artistImageElement.src = track.album.cover;
-}
-
-/** If the given guess is equal to the track name (disregarding case),
- * and there is not currently a correct guess, or the user has run out of guesses */
-function verifyGuess(guess, trackName) {
-    const correctGuessExists = isGuessLeft();
-
-    if (guess === trackName.toLowerCase() && !correctGuessExists) {
-        return true;
-    } else if (guess !== trackName.toLowerCase() && !correctGuessExists) {
-        return false;
-    }
-}
-
-/** Check if there is a correct-guess element.
- * This tells us that the user cannot guess again */
-function isGuessLeft() {
-    return document.querySelector(".correct-guess") !== null;
-}
---
+++ static/js/modules/gameManager.js
@@ -0,0 +1,50 @@
+class GameManager {
+    handleIncorrectGuess(firstGuessElement) {
+        firstGuessElement.className = "incorrect-guess";
+    }
+
+    /**Handles logic if user guesses correctly.
+     * Will firstly show a checkmark to indicate that the user has correctly guessed.
+     * Then, the function will reveal the track name and album to the user.
+     */
+    handleCorrectGuess(firstGuessElement, trackName, trackCoverUrl) {
+        const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                    </svg>`;
+
+        // Show a green check to indicate that the user made a correct guess
+        firstGuessElement.className = "correct-guess";
+        firstGuessElement.innerHTML = checkIcon;
+
+        this.showTrackInfo(trackName, trackCoverUrl);
+    }
+
+    /** Show the user the track name and album cover of the song. */
+    showTrackInfo(trackName, trackCoverUrl) {
+        const trackNameElement = document.getElementById("track-name");
+        const artistImageElement = document.getElementById("artist-image");
+
+        trackNameElement.textContent = trackName;
+        artistImageElement.src = trackCoverUrl;
+    }
+
+    /** If the given guess is equal to the track name (disregarding case),
+     * and there is not currently a correct guess, or the user has run out of guesses */
+    verifyGuess(guess, trackName) {
+        const correctGuessExists = this.isGuessLeft();
+
+        if (guess === trackName.toLowerCase() && !correctGuessExists) {
+            return true;
+        } else if (guess !== trackName.toLowerCase() && !correctGuessExists) {
+            return false;
+        }
+    }
+
+    /** Check if there is a correct-guess element.
+     * This tells us that the user cannot guess again */
+    isGuessLeft() {
+        return document.querySelector(".correct-guess") !== null;
+    }
+}
+
+export default GameManager;
