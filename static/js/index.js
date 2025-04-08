08692ba5f495280ad04aa6ac5bbc59a534ec1597
dastarruer <ayushpramanik399@gmail.com>
2025-04-07 20:15:38 -0400

    Move guess handling logic to its own function

--- static/js/index.js
+++ static/js/index.js
@@ -7,25 +7,9 @@ window.onload = async function () {
     const gameManager = new GameManager(3);
     await gameManager.startGame();
 
-    // Add ability to submit guess
+    // Handle guess when submit button is clicked
     submitButton.addEventListener("click", () => {
-        const firstGuessElement = document.querySelector(".guess");
-        const trackName = gameManager.player.track.title;
-        const trackCoverUrl = gameManager.player.track.album.cover;
-
-        // If the guess is valid
-        if (gameManager.verifyGuess(guessInput.value, trackName)) {
-            gameManager.handleCorrectGuess(
-                firstGuessElement,
-                trackName,
-                trackCoverUrl
-            );
-        } else {
-            // Check if firstGuessElement exists so that it doesn't interfere with other logic
-            if (firstGuessElement) {
-                gameManager.handleIncorrectGuess(firstGuessElement);
-            }
-        }
+        handleGuess(gameManager);
     });
 
     // Navigate suggestions with arrow keys
@@ -39,39 +23,44 @@ window.onload = async function () {
         // Unfocus the suggestions by resetting the index to its default
         gameManager.suggestionNavigator.currentSuggestionIndex = -1;
 
-        gameManager.trackMatcher.relevantTracks = gameManager.trackMatcher.getRelevantTracks(
-            guessInput.value
-        );
+        gameManager.trackMatcher.relevantTracks =
+            gameManager.trackMatcher.getRelevantTracks(guessInput.value);
 
         // Show suggestions to the user
-        gameManager.songSuggestions.showSuggestions(gameManager.trackMatcher.relevantTracks);
+        gameManager.songSuggestions.showSuggestions(
+            gameManager.trackMatcher.relevantTracks
+        );
     });
 
-    // Check if the user presses enter while focused on the input box, and verify guess
+    // Handle guess when enter is pressed on input box
     guessInput.addEventListener("keydown", () => {
         if (event.key === "Enter") {
-            const firstGuessElement = document.querySelector(".guess");
-            const trackName = gameManager.player.track.title;
-            const trackCoverUrl = gameManager.player.track.album.cover;
-
-            // If the guess is valid
-            if (gameManager.verifyGuess(guessInput.value, trackName)) {
-                gameManager.handleCorrectGuess(
-                    firstGuessElement,
-                    trackName,
-                    trackCoverUrl
-                );
-            } else {
-                // Check if firstGuessElement exists so that it doesn't interfere with other logic
-                if (firstGuessElement) {
-                    gameManager.handleIncorrectGuess(firstGuessElement);
-                }
-            }
+            handleGuess(gameManager);
         }
     });
-    // TODO: Restart without reloading page
-    // Restart button
+
+    // Restart game when restart button is clicked
     document.getElementById("restart-btn").addEventListener("click", () => {
         gameManager.startGame();
     });
 };
+
+/** Handle the guess differently depending on whether it is correct or
+ * incorrect using the hnadlceCorrectGuess() and handleIncorrectGuess()
+ * functions within the GameManager object. */
+function handleGuess(gameManager) {
+    const firstGuessElement = document.querySelector(".guess");
+    const trackName = gameManager.player.track.title;
+    const trackCoverUrl = gameManager.player.track.album.cover;
+
+    // If the guess is valid
+    if (gameManager.verifyGuess(guessInput.value, trackName)) {
+        gameManager.handleCorrectGuess(
+            firstGuessElement,
+            trackName,
+            trackCoverUrl
+        );
+    } else {
+        gameManager.handleIncorrectGuess(firstGuessElement);
+    }
+}
