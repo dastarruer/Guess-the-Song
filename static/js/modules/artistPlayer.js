8b5412b1ced6e8b3f80f555e5f4f3496c8584c2f
dastarruer <ayushpramanik399@gmail.com>
2025-04-11 21:40:13 -0400

    Add fade in and out on loop

--- static/js/modules/artistPlayer.js
+++ static/js/modules/artistPlayer.js
@@ -52,11 +52,37 @@ class ArtistPlayer {
 
         // Play the audio
         this.playPauseTrack();
-        this.fade();
+
+        // Fade in/out while looping
+        this.trackPlayer.addEventListener("timeupdate", () => {
+            const oneSecondLeft = this.trackPlayer.duration - 1;
+
+            if (
+                !this.track.paused &&
+                this.trackPlayer.currentTime >= oneSecondLeft
+            ) {
+                this.trackPlayer.currentTime = 0;
+                this.playTrack();
+            }
+        });
     }
 
     /** Play/pause the current song */
     playPauseTrack() {
+        if (this.trackPlayer.paused && !this.playing) {
+            // Fade in and play
+            this.playTrack();
+        } else {
+            // Fade out and pause
+            this.pauseTrack();
+        }
+    }
+
+    pauseTrack() {
+        const fadeDurationMs = 450;
+        const intervalMs = 50;
+        const step = 1 / (fadeDurationMs / intervalMs);
+
         const playIcon = `<!-- Play circle fill: https://icons.getbootstrap.com/icons/play-circle-fill/ -->
                         <svg
                             id="play-track-icon"
@@ -67,6 +93,27 @@ class ArtistPlayer {
                             <path
                                 d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
                         </svg>`;
+
+        const playPauseBtnElement = document.getElementById("play-track");
+
+        const fadeOut = setInterval(() => {
+            if (this.trackPlayer.volume > step) {
+                this.trackPlayer.volume -= step;
+            } else {
+                this.trackPlayer.volume = 0;
+                this.trackPlayer.pause();
+                clearInterval(fadeOut);
+            }
+        }, intervalMs);
+
+        playPauseBtnElement.innerHTML = playIcon;
+    }
+
+    playTrack() {
+        const fadeDurationMs = 450;
+        const intervalMs = 50;
+        const step = 1 / (fadeDurationMs / intervalMs);
+
         const pauseIcon = `<!-- Pause circle fill: https://icons.getbootstrap.com/icons/play-circle-fill/ -->
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -77,68 +124,23 @@ class ArtistPlayer {
                             <path
                                 d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5" />
                         </svg>`;
+
         const playPauseBtnElement = document.getElementById("play-track");
 
-        const fadeDurationMs = 450;
-        const intervalMs = 50;
-        const step = 1 / (fadeDurationMs / intervalMs);
+        this.trackPlayer.volume = 0;
+        this.trackPlayer.play();
 
-        if (this.trackPlayer.paused && !this.playing) {
-            // Fade in and play
-            this.trackPlayer.volume = 0;
-            this.trackPlayer.play();
+        const fadeIn = setInterval(() => {
+            console.log(this.trackPlayer.volume);
+            if (this.trackPlayer.volume < 1 - step) {
+                this.trackPlayer.volume += step;
+            } else if (!this.playing) {
+                this.trackPlayer.volume = 1;
+                clearInterval(fadeIn);
+            }
+        }, intervalMs);
 
-            const fadeIn = setInterval(() => {
-                if (this.trackPlayer.volume < 1 - step) {
-                    this.trackPlayer.volume += step;
-                } else if (!this.playing) {
-                    this.trackPlayer.volume = 1;
-                    clearInterval(fadeIn);
-                }
-            }, intervalMs);
-
-            playPauseBtnElement.innerHTML = pauseIcon;
-        } else {
-            // Fade out and pause
-            const fadeOut = setInterval(() => {
-                if (this.trackPlayer.volume > step) {
-                    this.trackPlayer.volume -= step;
-                } else {
-                    this.trackPlayer.volume = 0;
-                    this.trackPlayer.pause();
-                    clearInterval(fadeOut);
-                }
-            }, intervalMs);
-
-            playPauseBtnElement.innerHTML = playIcon;
-        }
-    }
-
-    fade() {
-        const audioLengthSecs = this.trackPlayer.duration;
-        const fadeLengthSecs = 5;
-
-        const volc = 1 / (fadeLengthSecs * 20); // 20 checks per second (50ms)
-
-        const currentTime = this.trackPlayer.currentTime;
-
-        const minVolume = 0;
-        const maxVolume = 1;
-        console.log(this.trackPlayer.volume);
-
-        if (currentTime + fadeLengthSecs >= audioLengthSecs) {
-            this.trackPlayer.volume = Math.max(
-                minVolume,
-                this.trackPlayer.volume - volc
-            );
-        } else if (currentTime <= fadeLengthSecs) {
-            this.trackPlayer.volume = Math.min(
-                maxVolume,
-                this.trackPlayer.volume + volc
-            );
-        }
-
-        setTimeout(() => this.fade(), 50);
+        playPauseBtnElement.innerHTML = pauseIcon;
     }
 }
 
