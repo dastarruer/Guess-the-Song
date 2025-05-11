cad4e9023a0546caad5087c7e6bc3f89b878f668
dastarruer <ayushpramanik399@gmail.com>
2025-05-11 15:12:18 -0400

    Add each genre to the dropdown menu

--- static/js/index.js
+++ static/js/index.js
@@ -7,9 +7,10 @@ const lives = 3;
 
 window.onload = async function () {
     let genres = await getGenres();
-    console.log(genres);
 
-    // const genreSuggestionManager = new GenreSuggestionManager(genres);
+    genres.forEach((genre) => {
+        addDropdown(genre);
+    });
 
     // const gameManager = new GameManager();
     // await gameManager.startGame(lives);
@@ -33,4 +34,23 @@ async function getGenres() {
     let data = await fetch("http://localhost:8080/genres");
     let genres = await data.json();
     return genres.data;
+}
+
+function addDropdown(genre) {
+    const dropdownMenu = document
+        .getElementById("genre-dropdown")
+        .querySelector(".dropdown-menu");
+    const li = document.createElement("li");
+    const btn = document.createElement("a");
+
+    btn.className = "dropdown-item"; // same Bootstrap style as <a>
+    btn.textContent = genre;
+    btn.type = "button"; // make it a button
+    btn.addEventListener("click", () => {
+        console.log(`Selected genre: ${genre}`);
+        // put your selection logic here
+    });
+
+    li.appendChild(btn);
+    dropdownMenu.appendChild(li);
 }
